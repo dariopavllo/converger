abd511b691bce754d633e071f8c6e4821431b720
Gabriele Graffieti <gabriele.graffieti@studio.unibo.it>
2015-02-13 22:35:59 +0100

    GUI first commit (the main part of the gui is almost finished), controller first commit

---
+++ src/org/converger/userinterface/gui/Header.java
@@ -0,0 +1,11 @@
+package org.converger.userinterface.gui;
+
+/**
+ * Represent the high part of the GUI.
+ * This part of the GUI provides buttons for manage the current environment (save, open, new, etc) and 
+ * for do operations on mathematical expression such derive or simplify.
+ * @author Gabriele Graffieti
+ */
+public interface Header extends GUIComponent {
+
+}
